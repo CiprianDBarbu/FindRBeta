fe5dba48f71010168474b65d210e0148a8794451
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2021-01-07 17:13:40 +0200

    Add project files.

---
+++ FindRBeta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindRBeta.Startup))]
+namespace FindRBeta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
