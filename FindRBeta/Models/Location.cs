6e42ea522d0560c5119ae50bfe94197a049f1ba2
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2021-01-07 17:20:41 +0200

    Added Profile and Location models

---
+++ FindRBeta/Models/Location.cs
@@ -0,0 +1,30 @@
+﻿namespace FindRBeta.Models
+{
+    enum Zone
+    {
+        Central,
+        Periferial
+    }
+    public class Location
+    {
+        private string country;
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+
+        private string city;
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+
+
+    }
+}
---
+++ FindRBeta/Models/Profile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindRBeta.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Others
+    }
+    public class Profile
+    {
+        public int Id { get; set; }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private Gender sex;
+
+        public Gender Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+
+
+        private Location address;
+
+        public Location Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        private string details;
+
+        public string Details
+        {
+            get { return details; }
+            set { details = value; }
+        }
+
+
+
+
+    }
+}
