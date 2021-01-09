3e93b7e952dad31521aaf58e31a69b7c269e3a75
Ciprian Barbu <barbu.ciprian.dragos@gmail.com>
2021-01-09 20:58:12 +0200

    CRUD controller added for Profile

--- FindRBeta/Controllers/ProfileController.cs
+++ FindRBeta/Controllers/ProfileController.cs
@@ -23,5 +23,105 @@ namespace FindRBeta.Controllers
 
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if(id.HasValue)
+            {
+                Profile profile = db.Profiles.Find(id);
+                if(profile != null)
+                {
+                    return View(profile);
+                }
+                return HttpNotFound("User does not exist!");
+            }
+            return HttpNotFound("Missing search details!");
+        }
+
+        [HttpGet]
+        public ActionResult New()
+        {
+            Profile profile = new Profile();
+
+            return View(profile);
+        }
+
+        [HttpPost]
+        public ActionResult New(Profile profileRequest)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Profiles.Add(profileRequest);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(profileRequest);
+            }
+            catch (Exception)
+            {
+                return View(profileRequest);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if(id.HasValue)
+            {
+                Profile profile = db.Profiles.Find(id);
+
+                if(profile == null)
+                {
+                    return HttpNotFound("User does not exist!");
+                }
+                return View(profile);
+            }
+            return HttpNotFound("Missing id parameter!");
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Profile profileRequest)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    Profile profile = db.Profiles.SingleOrDefault(p => p.ProfileId.Equals(id));
+
+                    if(TryUpdateModel(profile))
+                    {
+                        profile.Age = profileRequest.Age;
+                        profile.GenderType = profileRequest.GenderType;
+                        profile.Address = profileRequest.Address;
+                        profile.Details = profileRequest.Details;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+                return View(profileRequest);
+            }
+            catch (Exception)
+            {
+                return View(profileRequest);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            Profile profile = db.Profiles.Find(id);
+            if(profile != null)
+            {
+                db.Profiles.Remove(profile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return HttpNotFound($"Couldn't find the user with id {id}!");
+        }
+
     }
 }
